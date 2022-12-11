5c6e2884cec9ac347f642a58dba6bcdde046c0a8
miamania <73404401+miamania@users.noreply.github.com>
2022-12-11 16:59:09 -0500

    Mia's Binary Motion Detection

--- src/scripts/motion_detec.py
+++ src/scripts/motion_detec.py
@@ -11,34 +11,28 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the video file")
 args = vars(ap.parse_args())
 
-# if the video argument is None, then we are reading from webcam
-if args.get("video", None) is None:
-    vs = VideoStream(src=0).start()
-    time.sleep(2.0)
+# Read from video file
+vs = cv2.VideoCapture(args["video"])
 
-# otherwise, we are reading from a video file
-else:
-    vs = cv2.VideoCapture(args["video"])
-
-# initialize the first frame in the video stream
+# Initialize first frame
 firstFrame = None
 
-# loop over the frames of the video
+# Loop over frames
 while True:
-    # grab the current frame
+    # Get current frame
     frame = vs.read()
     frame = frame if args.get("video", None) is None else frame[1]
 
-    # if the frame could not be grabbed, then we have reached the end of the video
+    # Check for end of the video
     if frame is None:
         break
 
-    # resize the frame, convert it to grayscale, and blur it
+    # Resize, grayscale, and blur
     frame = imutils.resize(frame, width=500)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-    # if the first frame is None, initialize it
+    # If no first frame, initialize with basic gray image
     if firstFrame is None:
         firstFrame = gray
         continue
@@ -47,18 +41,19 @@ while True:
     frameDelta = cv2.absdiff(firstFrame, gray)
     thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-    # dilate the thresholded image to fill in holes, then find contours on thresholded image
+    # Dilate thresholded image
     thresh = cv2.dilate(thresh, None, iterations=2)
+    # Get countours
     cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
 
     cv2.imshow("Thresh", thresh)
     cv2.imshow("Frame Delta", frameDelta)
     key = cv2.waitKey(1) & 0xFF
-    # if the `q` key is pressed, break from the loop
-    if key == ord("q"):
+    # If the `e` key is pressed, break
+    if key == ord("e"):
         break
 
-# cleanup the camera and close any open windows
+# Close
 vs.stop() if args.get("video", None) is None else vs.release()
 cv2.destroyAllWindows()
